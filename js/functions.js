ebc0660551848b9b483b4da1166ec22e7a54fa2f
carles <carlesandreu4@gmail.com>
2021-05-20 12:10:26 +0200

    recom options test

--- js/functions.js
+++ js/functions.js
@@ -3,15 +3,18 @@ var webUrl = 'https://carles444.github.io/'
 var clientID = 'f3864cc367934305aac6f0bbf785b377';
 var clietnSecrt = 'ddbba0b3368f47ae8197e13e26c556b9';
 var Token = null;
+var option_basic_recom = '<button id="recommender_button" style="grid-area:layout_recommender" onclick="request_option(`basic`)"> BASIC RECOMMENDER </button>' 
+var create_list_by_artists = '<button id="recommender_button" style="grid-area:layout_recommender" onclick="request_option(`artists`)"> ARTISTS RECOMMENDER </button>' 
+
 
 currentUrl = window.location.href;
 if(currentUrl.includes('access_token')){
     pos = currentUrl.indexOf('=')
     Token = currentUrl.slice(pos+1, currentUrl.length)
+    el = document.getElementById('recommender_options')
+    el.innerHTML = option_basic_recom + create_list_by_artists;
+    
 }
-
-alert(Token);
-
 
 function authorization(){
     params = 'client_id='+clientID+'&response_type=token&redirect_uri='+webUrl;
