2bae93c3e531cca05d076c05e19971815da68cde
carles <carlesandreu4@gmail.com>
2021-05-21 11:44:38 +0200

    test request

--- js/functions.js
+++ js/functions.js
@@ -3,7 +3,7 @@ var webUrl = 'https://carles444.github.io/';
 var clientID = 'f3864cc367934305aac6f0bbf785b377';
 var clietnSecrt = 'ddbba0b3368f47ae8197e13e26c556b9';
 var Token = null;
-var server_url = 'https://europe-west1-graphite-hook-310809.cloudfunctions.net/RecoMusic';
+var server_url = 'https://europe-west1-graphite-hook-310809.cloudfunctions.net/RecoMusic/';
 
 currentUrl = window.location.href;
 
